d0996996deda2f4dde3039c36ba9b6f034305142
Ari F <24385099+arifeld@users.noreply.github.com>
2022-09-06 13:57:03 +1000

    Add fromData method to task list

--- Projects/task/tasks.js
+++ Projects/task/tasks.js
@@ -1,75 +1,75 @@
-class Task{
-    taskName = " ";
-    taskType = " ";
-    points;
-    status = "Not Started";
-    desc = "add description here";
-    tag;
-    assignee = [];
-    timeLog = 0;
-
-    constructor(taskName, taskType, points, status, description, tag){
-        this.taskName = taskName;
-        this.taskType = taskType;
-        this.points = points;
-        this.status = status;
-        this.desc = description;
-        this.tag = tag;
+class Task {
+    constructor(taskName="", taskType="", points=null, status=null, description="", tag=null, assignees=[] ){
+        this._taskName = taskName;
+        this._taskType = taskType;
+        this._points = points;
+        this._status = status;
+        this._desc = description;
+        this._tag = tag;
+        this._assignees = assignees;
     }
 
     addAssignee(newAssignee){
-        this.assignee.add(newAssignee);
+        this._assignees.add(newAssignee);
     }
 
     setTaskName(newTaskName){
-        this.taskName = newTaskName;
+        this._taskName = newTaskName;
     }
 
     setTaskType(newTaskType){
-        this.taskType = newTaskType;
+        this._taskType = newTaskType;
     }
 
     setPoints(newPoints){
-        this.points = newPoints;
+        this._points = newPoints;
     }
 
     setStatus(newStatus){
-        this.status = newStatus;
+        this._status = newStatus;
     }
 
     setDesc(newDesc){
-        this.desc = newDesc;    
+        this._desc = newDesc;    
     }
 
     setTag(newTag){
-        this.tag = newTag;
+        this._tag = newTag;
     }
 
     getTaskName(){
-        return this.taskName;
+        return this._taskName;
     }
     
     getTaskType(){
-        return this.taskType;
+        return this._taskType;
     }
 
     getPoints(){
-        return this.points;
+        return this._points;
     }
 
     getStatus(){
-        return this.status;
+        return this._status;
     }
 
     getDesc(){
-        return this.desc;
+        return this._desc;
     }
 
     getTag(){
-        return this.tag;
+        return this._tag;
     }
 
     getTimeLog(){
-        return this.timeLog;
+        return this._timeLog;
+    }
+
+    fromData(data){
+        users = data._assignees.forEach((user) => {
+            const newUser = new User(user._name, user._email, user._timeLog)
+        }) 
+        return new Task(taskName=data._taskName, taskType=data._taskType, points=data._points, status=data._status, description=data._description, tag=data._tag, assignees=data._assignees)
+
     }
 }
